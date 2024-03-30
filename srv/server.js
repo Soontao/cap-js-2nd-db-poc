48a408134dc63c8af6ea61c93964c3effdc11e7d
Theo Sun <theo.sun.github@outlook.com>
2024-03-30 11:54:15 +0800

    chore: update

--- srv/server.js
+++ srv/server.js
@@ -1,3 +1,14 @@
-const cds = require("@sap/cds");
+import cds from "@sap/cds";
 
 cds.on("bootstrap", () => {});
+
+cds.on("serving:cds.xt.DeploymentService", (ds) => {
+  ds.prepend((ds) => {
+    ds.after("deploy", async () => {
+      const db2 = await cds.connect.to("db2");
+      await db2.run(
+        "CREATE TABLE MyTable (ID INT PRIMARY KEY, Name VARCHAR(255))",
+      );
+    });
+  });
+});
